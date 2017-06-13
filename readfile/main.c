2e7a30b4ae7174c8b54ba5660c6a712a470c33b2
20161104616 <992846873@qq.com>
2017-06-13 11:41:02 +0800

    读取文件1的10个数排序到文件2
    读取文件1的10个数排序到文件2

---
+++ readfile/main.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int n[10],i,j,a;
+    FILE *fr;
+    FILE *fw;
+    fr = fopen("//Users//a20161104616//Desktop//readfile//readfile//sort1.txt","r+");
+    fw = fopen("//Users//a20161104616//Desktop//readfile//readfile//sort2.txt","w");
+    if(fr == NULL)
+        {
+            printf("error");
+            return -1;
+        }
+    else
+        {
+            for(i=0;i<10;i++)
+                fscanf(fr,"%d",&n[i]);
+            for(i=0;i<10;i++)
+            {
+                for(j=0;j<9-i;j++)
+                {
+                    if(n[j]>n[j+1])
+                    {
+                        a=n[j];
+                        n[j]=n[j+1];
+                        n[j+1]=a;
+                    }
+                }
+            }
+        }
+    for (i=0; i<10; i++)
+    {
+        printf("%d\t",n[i]);
+    }
+    printf("\n");
+    fprintf(fw, "%d\t",n[i]);
+    return 0;
+}
